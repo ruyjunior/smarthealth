24a4a2f79bef83090e0f3276689ea20806f8ef14
Ruy Junior <ruyjunior21@gmail.com>
2025-05-17 23:59:27 -0300

    Correção de imagem e frase principal

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,13 +1,11 @@
 import Hero from "./site/sections/hero";
 import Services from "./site/sections/services";
 import About from "./site/sections/about";
-import Clients from "./site/sections/clients";
 import { Metadata } from 'next';
-import CallToAction from "./site/sections/call_to_action";
 
 export const metadata: Metadata = {
-  title: 'HOME',
-  description: 'Smart Health - A saúde na palma da sua mão',
+  title: 'Smart Health',
+  description: 'Sua clínica online, na palma da sua mão!',
 };
 
 export default async function Page() {
